39065f58b432f5d51e6f51e52b3f8026edf5d61f
vanhuutoan27 <vhtoan27@gmail.com>
2023-11-01 14:30:27 +0700

    add new vps

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 echo "Building app..."
 npm run build
 echo "Deploy files to server..."
-scp -r -i ~/Desktop/SWP391/4stu build/* root@157.245.196.103:/var/www/html/
+# scp -r -i ~/Desktop/SWP391/4stu build/* root@157.245.196.103:/var/www/html/
+scp -r -i ~/Desktop/SWP391/4stu build/* root@143.198.194.141:/var/www/html/
 echo "Done!"
